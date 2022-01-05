44b0527f5d6510719b7fe603512a00fe615b2102
Luis Jorge <luis.jorge@inm.pt>
2022-01-05 13:18:50 +0000

    first commit and finished simple slider in vue3

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="container">
+		<img alt="Vue logo" src="./assets/logo.png" />
+		<Slider v-model="sliderValue" label="Slider" min="0" max="10" prefix symbol="€"/>
+	</div>
+</template>
+
+<script>
+import Slider from './components/Slider.vue'
+
+export default {
+	name: 'App',
+	components: {
+		Slider
+	},
+	data() {
+		return {
+			sliderValue: null
+		}
+	},
+}
+</script>
---
+++ src/components/Slider.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class='slider wrapper'>
+		<label class="amount">
+			<div v-if="prefix" class="dollar-sign">{{symbol}}</div>
+			<div class="dollars" :for='"slide-"+$id'>{{displayValue}}</div>
+			<div v-if="!prefix" class="dollar-sign">{{symbol}}</div>
+		</label>
+		<input type='range' :id="$id" :name='"slide-"+$id' :min="min" :max="max" step="0.01" @input="handleSlideInput"/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Slider',
+	props: {
+		label: String,
+		symbol: String,
+		prefix: Boolean,
+		min: Number,
+		max: Number,
+		modelValue: Number
+	},
+	data () {
+		return {
+		}
+
+	},
+	computed:{
+		displayValue(){
+			return Number(this.modelValue?? this.max).toFixed(2)
+		}
+	},
+	methods:{
+		handleSlideInput(ev){
+			console.log(ev.target.value)
+			this.$emit('update:modelValue', ev.target.value)
+		}
+	}
+}
+</script>
+<style lang="css" scoped>
+	.slider{
+		display:flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+</style>
