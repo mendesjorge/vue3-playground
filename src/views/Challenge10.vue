b1d3ada7aa09ddb3f2891eeb11d4febce973d5d9
Luis Jorge <luis.jorge@inm.pt>
2022-01-27 17:36:51 +0000

    trying make like card rotation as response of failed or success at verifying

--- src/views/Challenge10.vue
+++ src/views/Challenge10.vue
@@ -1,17 +1,22 @@
 <template>
 	<div class="challenge-10-wrapper">
-		<h1>Authorization Code</h1>
-		<p>Please enter the code that we sent to (***) *** - 2819.</p>
-		<form action="">
-			<div class="fields">
+		<div class="container" :class="{'rotate': rotateCard}">
+			<h1>Authorization Code</h1>
+			<p>Please enter the code that we sent to (***) *** - 2819.</p>
+			<form>
+				<div class="fields">
 				<input type="text" maxlength="1" v-model="numbers[0]" />
 				<input type="text" maxlength="1" v-model="numbers[1]" />
 				<input type="text" maxlength="1" v-model="numbers[2]" />
 				<input type="text" maxlength="1" v-model="numbers[3]" />
-			</div>
-			<button>Verify</button>
-		</form>
-		{{code}}
+				</div>
+				<button @click="verifyButtonHandler">Verify</button>
+			</form>
+		</div>
+		<div class="container-back-card" :class="{'success':hasSucceed, 'fail': hasFailed}">
+			{{hasSucceed? 'success':'fail'}}
+			<button @click="rotateCard = false">Try Again</button>
+		</div>
 	</div>
 </template>
 
@@ -19,10 +24,20 @@
 import { ref, computed } from 'vue'
 
 const numbers = ref(['','','',''])
-
 const code = computed(() =>
 	numbers.value.reduce((prev,cur) => prev+cur)
 )
+
+let success = ref(false)
+const rotateCard = ref(false)
+
+const hasSucceed = computed(()=> success.value && rotateCard.value)
+const hasFailed = computed(()=> !success.value && rotateCard.value)
+
+const verifyButtonHandler = () => {
+	success.value = code === '1234'
+	rotateCard.value = true
+}
 
 </script>
 
@@ -30,13 +45,50 @@ const code = computed(() =>
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;900&display=swap');
 
 .challenge-10-wrapper {
-  background: white;
-  box-shadow: 0px 23px 44px #CAD0E0;
-  border-radius: 10px;
-  min-width: 575px;
-  padding: 70px 35px 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+}
+.container, .container-back-card{
+	background: white;
+	box-shadow: 0px 23px 44px #CAD0E0;
+	border-radius: 10px;
+	min-width: 575px;
+	height: 300px;
+	padding: 70px 35px 30px;
+}
+.container{
+	transition: transform 1s ease-in-out, visibility 0s 1s ease-in-out;
+}
+.rotate{
+	transform: rotateY(90deg);
+	visibility: hidden;
+}
+.container-back-card{
+	display:flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	transform: rotateY(-90deg);
+	visibility: hidden;
+	transition: visibility 1s 1s ease-in-out, transform 1s 1s ease-in-out;
+	transition-delay: 1s;
 }
 
+.container-back-card.success{
+	background-color: lightgreen;
+	transform: rotateY(0deg);
+	visibility: visible;
+}
+.container-back-card.fail{
+	background-color: lightgreen;
+	transition-delay: 1s;
+	transform: rotateY(0deg);
+	visibility: visible;
+}
 h1 {
   text-align: center;
   letter-spacing: 0.11em;
