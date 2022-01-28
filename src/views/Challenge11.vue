a39455c81a4998d54b966b598bdd6b01d4214e4c
Luis Jorge <luis.jorge@inm.pt>
2022-01-28 16:07:10 +0000

    finished challenge 11, no need of vue here but handler is declared on vue setup

--- src/views/Challenge11.vue
+++ src/views/Challenge11.vue
@@ -1,22 +1,174 @@
 <template>
-	<div class="challenge-10-wrapper">in development</div>
+	<div class="challenge-11-wrapper">
+		<div class="container">
+			<ul>
+				<li @click="toggleQuestionHandler">
+					<div class="question">
+						<div class="question-mark">
+						<img :src="'./challenge11/question-1.svg'" alt="question" />
+						</div>
+						<div class="content">Technically, Svelte is a library and SvelteKit is a framework. What's the difference?
+						</div>
+						<img class="chevron" :src="'/challenge11/chevron.svg'" alt="chevron">
+					</div>
+					<div class="answer">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna
+						aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
+					</p>
+
+					<p>
+						Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+						sunt in
+						culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					</div>
+				</li>
+
+				<li class="expand" @click="toggleQuestionHandler">
+					<div class="question">
+						<div class="question-mark">
+						<img :src="'./challenge11/question-2.svg'" alt="question" />
+						</div>
+						<div class="content">Do you provide a certificate of completion?</div>
+						<img class="chevron" :src="'/challenge11/chevron.svg'" alt="chevron">
+
+					</div>
+					<div class="answer">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna
+						aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
+					</p>
+
+					<p>
+						Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+						sunt in
+						culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
-<script>
-export default {
+<script setup>
 
+const toggleQuestionHandler = (e) => {
+	e.currentTarget.classList.toggle('expand')
 }
+
 </script>
 
 <style scoped>
-	.challenge-10-wrapper{
-		width:100%;
-		height: 100%;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+	@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&display=swap');
 
-		font-size: 40px;
-	}
+.challenge-11-wrapper{
+	width:100%;
+	height: 100%;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-family: 'Open Sans', sans-serif;
+	font-weight: 800;
+}
+
+.container {
+  max-width: 1010px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  margin-bottom: 75px;
+}
+
+a {
+  text-decoration: none;
+}
+
+.question {
+  background: white;
+  border: 5px solid black;
+  padding: 25px 38px;
+  width: calc(100% - 55px);
+  display: block;
+  position: relative;
+  padding-left: 130px;
+  box-sizing: border-box;
+  color: black;
+}
+
+.expand .question:after{
+	content: '';
+	background-color: #FFDD00;
+	width: calc(100%);
+	position: absolute;
+	right: -55px;
+	top: 15px;
+	height: calc(100% + 15px);
+	z-index: -1;
+
+}
+
+.question:before {
+  content: '';
+  display: block;
+  background: black;
+  width: 102px;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 2;
+}
+
+.question-mark {
+  position: absolute;
+  left: 10px;
+  z-index: 5;
+  top: -45px;
+}
+
+.chevron {
+	/* width:;
+	height: 20px; */
+  /* background: #F0F0F0 url('/challenge11/chevron.svg') calc(100% - 12px) 20px no-repeat; */
+  /* width: calc(100%); */
+  position: absolute;
+  left: calc(100% + 12px);
+  top: 20px;
+  /* height: calc(100% + 15px); */
+  z-index: 1;
+}
+
+.expand .question .chevron,
+a:hover .question .chevron{
+  transform: rotateX(180deg);
+}
+
+.question .content {
+  position: relative;
+  z-index: 3;
+}
+
+.answer {
+  display: none;
+}
+
+.expand .answer {
+  display: block;
+  background-color: #FFDD00;
+  width: calc(100% - 60px);
+  margin-left: 55px;
+  padding: 30px 75px;
+  box-sizing: border-box;
+}
 </style>
